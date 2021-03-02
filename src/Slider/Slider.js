b981d9b352b1408750a140588d1820431551eef8
Edward116 <78700438+Edward116@users.noreply.github.com>
2021-03-02 12:16:24 +0100

    second commit

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -10,19 +10,40 @@ const Slider = (props) => {
         {url: 'https://designstil.info/wp-content/uploads/2019/03/Goluboj-tsvet-sten-1.png'},
         {url: 'https://impicode.pl/wp-content/uploads/2019/09/przyklady-aplikacji-napisanych-w-react-native-1.jpg'}
     ]
-
-
     const [current, setCurrent] = useState(0);
+
+    let newsText = [
+        {txt:'First Page', news:'News on Page 1'},
+        {txt:'Second Page', news:'News on Page 2'},
+        {txt:'Third Page', news: 'News on Page 3'}
+    ]
+
+
+    const [text, setText] = useState(0);
     const length = images.length;
+
 
 
     const prev = () => {
         setCurrent(current === 0 ? length - 1 : current - 1)
+        setText(text===0 ? newsText.length - 1 : text - 1)
     }
+
 
     const next = () => {
         setCurrent(current === length - 1 ? 0 : current + 1)
+        setText(text===newsText.length - 1 ? 0 : text + 1)
     }
+
+    let textElement = newsText.map((tex, index) => {
+        return(<div key={index}>
+            {index === text && (<div>
+                <h2>{tex.txt}</h2>
+                <p className='news'>{tex.news}</p>
+                    </div>)}
+                </div>
+        )
+    })
 
 
     let buttonElement = images
@@ -31,25 +52,29 @@ const Slider = (props) => {
     let imageElement = images.map((img, index) => {
         return (
             <div className={index === current ? 'slide active' : 'slide'}
-            key={index}>
+                 key={index}>
                 {index === current && (<img src={img.url}/>)}
-            </div>
-        )
+                </div>);
     })
 
 
-    return (
-        <div>
-            <div className='slider'>
-                <button className='prev' onClick={prev}> 0 </button>
-                {imageElement}
-                <button className='next' onClick={next}> > </button>
-            </div>
-            <div className='navigation'>
-                {buttonElement}
-            </div>
-        </div>
-    );
-}
+                return (
+                <div>
+                    <div className='slider'>
+                        <button className='prev' onClick={prev}> 0</button>
+                        <div>
+                        {imageElement}
+                        <div className='text'>
+                            {textElement}
+                        </div>
+                        </div>
+                        <button className='next' onClick={next}> ></button>
+                    </div>
+                    <div className='navigation'>
+                        {buttonElement}
+                    </div>
+                </div>
+                )}
 
-export default Slider
+
+                export default Slider
